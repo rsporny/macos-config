0ae81acf3be216290a978e7e67f3e34287129d57
Radosław Sporny <404@rspo.dev>
2025-08-19 17:03:24 +0200

    add: nvim line numbers

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -10,3 +10,15 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
 })
 
+vim.api.nvim_create_augroup('NumberToggle', { clear = true })
+vim.api.nvim_create_autocmd('InsertEnter', {
+  group = 'NumberToggle',
+  pattern = '*',
+  command = 'set number',
+})
+vim.api.nvim_create_autocmd('InsertLeave', {
+  group = 'NumberToggle',
+  pattern = '*',
+  command = 'set relativenumber',
+})
+
