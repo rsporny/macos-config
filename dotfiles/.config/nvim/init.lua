54b37b1d6d3f36a45a6df7a0358bb4cde83c6810
Radosław Sporny <404@rspo.dev>
2025-08-26 10:29:27 +0200

    add: conform to manage formatters (lua, json)

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -10,15 +10,52 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
 })
 
-vim.api.nvim_create_augroup('NumberToggle', { clear = true })
-vim.api.nvim_create_autocmd('InsertEnter', {
-  group = 'NumberToggle',
-  pattern = '*',
-  command = 'set number',
+--------------------------------------------------
+-- number and relative number
+--------------------------------------------------
+local NumberToggle = vim.api.nvim_create_augroup("NumberToggle", { clear = true })
+vim.api.nvim_create_autocmd({ "VimEnter", "BufReadPost", "BufNewFile" }, {
+  group = NumberToggle,
+  pattern = "*",
+  callback = function()
+    vim.wo.number = true
+    vim.wo.relativenumber = true
+  end,
 })
-vim.api.nvim_create_autocmd('InsertLeave', {
-  group = 'NumberToggle',
-  pattern = '*',
-  command = 'set relativenumber',
+vim.api.nvim_create_autocmd("InsertEnter", {
+  group = NumberToggle,
+  pattern = "*",
+  callback = function()
+    vim.wo.relativenumber = false
+    vim.wo.number = true
+  end,
+})
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = NumberToggle,
+  pattern = "*",
+  callback = function()
+    vim.wo.relativenumber = true
+  end,
 })
 
+--------------------------------------------------
+-- vim-commentary
+--------------------------------------------------
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.envrc.local", "*.envrc" },
+  command = "setfiletype sh",
+})
+
+--------------------------------------------------
+-- conform formatter
+--------------------------------------------------
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf, verbose = true })
+  end,
+})
+vim.keymap.set("n", "<leader>f", function()
+  -- `bufnr = vim.api.nvim_get_current_buf()` is the same value you passed in the autocmd
+  require("conform").format({ bufnr = vim.api.nvim_get_current_buf() })
+end, { desc = "Format buffer with Prettier" })
